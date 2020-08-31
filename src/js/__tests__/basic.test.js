986c72d1a8e560a6e0f616452b31f8023a72e24e
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-08-31 21:33:55 +0300

    competed full task

---
+++ src/js/__tests__/basic.test.js
@@ -0,0 +1,21 @@
+import { shortDesc } from '../app';
+
+describe('check function shortDesc', () => {
+  test('return corrected message', () => {
+    const expected = '😠Л(1)⚔40🛡10❤50';
+    const received = shortDesc({
+      name: 'Лучник',
+      type: 'Bowman',
+      health: 50,
+      level: 1,
+      attack: 40,
+      defence: 10,
+    });
+    expect(received).toEqual(expected);
+  });
+  test('return null', () => {
+    const expected = null;
+    const received = shortDesc();
+    expect(received).toEqual(expected);
+  });
+});
---
+++ src/js/app.js
@@ -0,0 +1,51 @@
+// TODO: write your code here
+
+const { String } = require('core-js');
+
+const profileHero = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 1,
+  attack: 40,
+  defence: 10,
+};
+
+const shortDesc = (obj) => {
+  if (obj === undefined) return null;
+  const {
+    name, type, health, level, attack, defence,
+  } = obj;
+  const firstSymbolName = name.charAt(0);
+  let typeSymbol;
+  if (type === 'Bowman') {
+    typeSymbol = String.fromCodePoint(0x1F620);
+  } if (type === 'Swordsman') {
+    typeSymbol = String.fromCodePoint(0x1F621);
+  } if (type === 'Magician') {
+    typeSymbol = String.fromCodePoint(0x1F9D0);
+  } if (type === 'Daemon') {
+    typeSymbol = String.fromCodePoint(0x1F47F);
+  } if (type === 'Undead') {
+    typeSymbol = String.fromCodePoint(0x1F480);
+  } if (type === 'Zombie') {
+    typeSymbol = String.fromCodePoint(0x1F9D);
+  }
+  const attackSymbol = String.fromCodePoint(0x2694);
+  const defenceSymbol = String.fromCodePoint(0x1F6E1);
+  const healthSymbol = String.fromCodePoint(0x2764);
+  const message = `${typeSymbol}${firstSymbolName}(${level})${attackSymbol}${attack}${defenceSymbol}${defence}${healthSymbol}${health}`;
+  console.log(message);
+  return message;
+};
+
+shortDesc({
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 1,
+  attack: 40,
+  defence: 10,
+});
+
+export { shortDesc };
